26af5361dd46f9f410c88ee9db3de15c47a3108b
Bhk <bharatraghunthan9767@gmail.com>
2018-01-01 12:36:52 +0530

    Added getter and setter methods and private variable conventions

--- RPG.py
+++ RPG.py
@@ -1,25 +1,34 @@
 class Character():
+	'''Assuming name is public, health and damage are private variables'''
     def __init__(self,damage,health,name,**kwargs):
-        self.damage = damage
-        self.health = health
+        self._damage = damage
+        self._health = health
         self.name = name
+    '''Getter and Setter Methods'''    
+    def GetHealth()
+    	return self._health
+    def GetDamage()
+    	return self._damage
+    def SetHealth(health):
+    	self._health = health
+    def	SetDamage(damage):
+    	self._damage = damage		
     def Attack(self,enemy):
-        enemy.health -= self.damage
-        if enemy.health <= 0:
-        	print(enemy.name + " Wasted")
+        enemy.Take_damage(self.GetDamage())
     def Take_damage(self,damage):
-        if self.health <= damage:
+        if self.GetHealth() <= damage:
         	print(self.name + " Wasted")
         else:		
-        	self.health -= damage
+        	self.GetHealth() -= damage
 class Hero(Character):
+	'''max_health is a public var for now '''
     def __init__(self,damage=10,health=30,max_health=50,name="Hero",**kwargs):
         self.max_health = max_health
         super().__init__(damage,health,name,**kwargs)
     def rest(self):
-        self.health += self.damage
-        if self.health > max_health:
-        	self.health = max_health
+        self.GetHealth() += self.GetDamage()
+        if self.GetHealth() > max_health:
+        	self.GetHealth() = max_health
 class Monster(Character):
 	def __init__(self,damage,health,name,**kwargs):
 		super().__init__(damage,health,name,**kwargs)	        	
