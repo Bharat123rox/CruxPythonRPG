df301dac02e06f9750b06dc94f218c62cdf54d7a
Bhk <bharatraghunthan9767@gmail.com>
2018-01-01 10:21:45 +0530

    Added some minor changes to the template of the RPG

--- RPG.py
+++ RPG.py
@@ -1,27 +1,31 @@
 class Character():
-    def __init__(self,Damage,Health,name,**kwargs):
-        self.Damage = Damage
-        self.Health = Health
+    def __init__(self,damage,health,name,**kwargs):
+        self.damage = damage
+        self.health = health
         self.name = name
     def Attack(self,enemy):
-        enemy.Health -= self.Damage
-        if enemy.Health <= 0:
-        	print(enemy.name + "Wasted")
-    def Take_damage(self,Damage):
-        self.Health -= Damage
-        if self.Health <= 0:
-        	print(self.name + "Wasted")	
+        enemy.health -= self.damage
+        if enemy.health <= 0:
+        	print(enemy.name + " Wasted")
+    def Take_damage(self,damage):
+        if self.health <= damage:
+        	print(self.name + " Wasted")
+        else:		
+        	self.health -= damage
 class Hero(Character):
-    def __init__(self,Damage=10,Health=30,MAX_HEALTH=50,name="Hero",**kwargs):
-        self.MAX_HEALTH = MAX_HEALTH
-        super().__init__(Damage,Health,name,**kwargs)
+    def __init__(self,damage=10,health=30,max_health=50,name="Hero",**kwargs):
+        self.max_health = max_health
+        super().__init__(damage,health,name,**kwargs)
     def rest(self):
-        self.Health += self.Damage
-        if self.Health > MAX_HEALTH:
-        	self.Health = MAX_HEALTH
-class Goblin(Character):
-    def __init__(self,Damage=5,Health=10,name="Goblin",**kwargs):
-        super().__init__(Damage,Health,name,**kwargs)
-class Orc(Character):
-    def __init__(self,Damage=10,Health=20,name="Orc",**kwargs):
-        super().__init__(Damage,Health,name,**kwargs)
+        self.health += self.damage
+        if self.health > max_health:
+        	self.health = max_health
+class Monster(Character):
+	def __init__(self,damage,health,name,**kwargs):
+		super().__init__(damage,health,name,**kwargs)	        	
+class Goblin(Monster):
+    def __init__(self,damage=5,health=10,name="Goblin",**kwargs):
+        super().__init__(damage,health,name,**kwargs)
+class Orc(Monster):
+    def __init__(self,damage=10,health=20,name="Orc",**kwargs):
+        super().__init__(damage,health,name,**kwargs)
